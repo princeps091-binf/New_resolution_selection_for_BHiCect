13720502f8d2f100f582f3fbdf8d0e6c1ebc202b
princeps091-binf <princeps091@gmail.com>
2022-01-03 14:55:18 +0100

    reconciling raw and KR data

---
+++ code/rao_criteria_trial.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(Matrix)
+options(scipen=999999999)
+#############################################################
+res_set<-c('1Mb','500kb','100kb','50kb','10kb','5kb')
+res_num<-c(1e6,5e5,1e5,5e4,1e4,5e3)
+names(res_num)<-res_set
+KR_dat_file<-'~/Documents/multires_bhicect/data/GM12878/'
+raw_dat_file<-'~/Documents/multires_bhicect/data/GM12878/raw/'
+
+chromo<-"chr22"
+
+chr_dat_l<-lapply(res_set,function(x){
+  
+  KR_dat<-read_delim(file = paste0(KR_dat_file,x,'/',chromo,'.txt'),delim = '\t',col_names = F)
+  raw_dat<-read_delim(file = paste0(raw_dat_file,x,'/',chromo,'.txt'),delim = '\t',col_names = F)
+  chr_dat<-KR_dat %>% dplyr::rename(KR=X3) %>% left_join(.,raw_dat%>% dplyr::rename(raw=X3)) %>% mutate(res=x)
+  
+  })
+
+chr_dat_l<-lapply(chr_dat_l,function(x){
+  x%>%filter(!(is.nan(KR)))
+})
+
+
+chr_dat_l[[6]] %>% ggplot(.,aes(KR,raw))+geom_point()
+
+
